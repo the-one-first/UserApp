a83dd3e6962c5219e14e6ba20684247abeeae543
the-one-first <owen.hadi10@gmail.com>
2020-02-19 15:31:51 +0700

    Initial commit

---
+++ src/test/java/com/wirecard/userapp/UserAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wirecard.userapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
