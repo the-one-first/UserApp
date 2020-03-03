7d0083a25feb75c81d1f99d742041c91595180f8
the-one-first <owen.hadi10@gmail.com>
2020-03-03 09:37:50 +0700

    Remove Unused Import
    Remove JpaSpecificationExecutor and PagingAndSortingRepository in Repo

--- src/main/java/com/wirecard/userapp/usertype/repository/UserTypeRepository.java
+++ src/main/java/com/wirecard/userapp/usertype/repository/UserTypeRepository.java
@@ -1,12 +1,9 @@
 package com.wirecard.userapp.usertype.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.wirecard.userapp.usertype.entity.UserType;
 
-public interface UserTypeRepository extends JpaRepository<UserType, Long>, PagingAndSortingRepository<UserType, Long>,
-        JpaSpecificationExecutor<UserType>, CustomUserTypeRepository {
+public interface UserTypeRepository extends JpaRepository<UserType, Long>, CustomUserTypeRepository {
 
 }
